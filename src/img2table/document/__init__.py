3c6baaf089aafcf1536623903aa19b87df71cc27
Xavier C <xav.canton@gmail.com>
2023-01-02 19:03:15 +0100

    Create classes for OCR

--- src/img2table/document/__init__.py
+++ src/img2table/document/__init__.py
@@ -1,14 +1,21 @@
 # coding: utf-8
 from dataclasses import dataclass
+from enum import Enum, auto
 from io import BytesIO
 from typing import Union, Iterator
 
 import numpy as np
 
 
+class DocumentType(Enum):
+    PDF = auto()
+    IMAGE = auto()
+
+
 @dataclass
 class Document:
     src: Union[str, BytesIO, bytes]
+    typ: DocumentType
     dpi: int = 300
 
     @property
