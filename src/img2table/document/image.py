2c4ebebef43ad1233dcaf24c9c7991ad5de6d7c9
Xavier C <xav.canton@gmail.com>
2023-01-09 23:13:24 +0100

    Update default DPI for images

--- src/img2table/document/image.py
+++ src/img2table/document/image.py
@@ -8,6 +8,8 @@ from img2table.document import Document
 
 
 class Image(Document):
+    dpi: int = 200
+
     @property
     def images(self) -> Iterator[np.ndarray]:
         yield cv2.imdecode(np.frombuffer(self.bytes, np.uint8), cv2.IMREAD_GRAYSCALE)
