09f5a5627ae2ce05c9fd4fe3d2cfdbc5dc3cdf6e
Xavier C <xav.canton@gmail.com>
2023-01-09 23:51:25 +0100

    Add HTML representation for extracted tables

--- src/img2table/tables/objects/extraction.py
+++ src/img2table/tables/objects/extraction.py
@@ -28,5 +28,24 @@ class ExtractedTable:
 
     @property
     def df(self) -> pd.DataFrame:
+        """
+        Create pandas DataFrame representation of the table
+        :return: pandas DataFrame containing table data
+        """
         values = [[cell.value for cell in row] for k, row in self.content.items()]
         return pd.DataFrame(values)
+
+    def html_repr(self, title: Optional[str] = None) -> str:
+        """
+        Create HTML representation of the table
+        :param title: title of HTML paragraph
+        :return: HTML string
+        """
+        html = f"""{rf'<h3 style="text-align: center">{title}</h3>' if title else ''}
+                   <p style=\"text-align: center\">
+                       <b>Title:</b> {self.title or 'No title detected'}<br>
+                       <b>Bounding box:</b> x1={self.bbox.x1}, y1={self.bbox.y1}, x2={self.bbox.x2}, y2={self.bbox.y2}
+                   </p>
+                   <div align=\"center\">{self.df.to_html()}</div>
+                """
+        return html
