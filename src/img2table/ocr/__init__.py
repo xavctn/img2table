ee9a6e974ba0753ee3657d3c43c7390e7c2e306f
Xavier C <xav.canton@gmail.com>
2023-02-09 21:13:12 +0100

    Fix import with missing libraries

--- src/img2table/ocr/__init__.py
+++ src/img2table/ocr/__init__.py
@@ -1,6 +1,18 @@
 # coding: utf-8
 
-from img2table.ocr.aws_textract import TextractOCR
-from img2table.ocr.azure import AzureOCR
-from img2table.ocr.google_vision import VisionOCR
+try:
+    from img2table.ocr.aws_textract import TextractOCR
+except ImportError:
+    pass
+
+try:
+    from img2table.ocr.azure import AzureOCR
+except ImportError:
+    pass
+
+try:
+    from img2table.ocr.google_vision import VisionOCR
+except ImportError:
+    pass
+
 from img2table.ocr.tesseract import TesseractOCR
