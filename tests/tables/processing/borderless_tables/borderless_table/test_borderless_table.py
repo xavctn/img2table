d5efff3fcfba108435e9c93c7fb3806e5aa7ed72
Xavier C <xav.canton@gmail.com>
2023-03-28 00:00:19 +0200

    Add tests on borderless table creation

---
+++ tests/tables/processing/borderless_tables/borderless_table/test_borderless_table.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+import json
+
+import cv2
+import polars as pl
+
+from img2table.ocr.data import OCRDataframe
+from img2table.tables.objects.line import Line
+from img2table.tables.processing.borderless_tables import identify_borderless_tables
+
+
+def test_identify_borderless_tables():
+    img = cv2.imread("test_data/test.png", cv2.IMREAD_GRAYSCALE)
+    ocr_df = OCRDataframe(df=pl.read_csv("test_data/ocr_df.csv", sep=";").lazy())
+
+    with open("test_data/lines.json", 'r') as f:
+        data = json.load(f)
+    lines = [Line(**el) for el in data.get('h_lines') + data.get('v_lines')]
+
+    result = identify_borderless_tables(img=img,
+                                        ocr_df=ocr_df,
+                                        lines=lines,
+                                        existing_tables=[])
+
+    assert len(result) == 2
+    assert (result[0].nb_columns, result[0].nb_rows) == (3, 6)
+    assert (result[1].nb_columns, result[1].nb_rows) == (2, 2)
---
+++ tests/tables/processing/borderless_tables/table/test_table.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+from img2table.tables.objects.cell import Cell
+from img2table.tables.objects.line import Line
+from img2table.tables.objects.row import Row
+from img2table.tables.objects.table import Table
+from img2table.tables.processing.borderless_tables import identify_table
+from img2table.tables.processing.borderless_tables.model import LineGroup, TableLine
+
+
+def test_identify_table():
+    line_group = LineGroup(lines=[TableLine(cells=[Cell(x1=0, x2=30, y1=0, y2=10)]),
+                                  TableLine(cells=[Cell(x1=0, x2=30, y1=10, y2=20)]),
+                                  TableLine(cells=[Cell(x1=0, x2=30, y1=20, y2=30)]),
+                                  TableLine(cells=[Cell(x1=0, x2=30, y1=30, y2=40)]),
+                                  TableLine(cells=[Cell(x1=0, x2=30, y1=40, y2=100)])])
+
+    column_delimiters = [Cell(x1=5, x2=15, y1=0, y2=100), Cell(x1=15, x2=25, y1=0, y2=100)]
+
+    elements = [Cell(x1=0, x2=10, y1=0, y2=10),
+                Cell(x1=10, x2=20, y1=10, y2=20),
+                Cell(x1=0, x2=10, y1=20, y2=30), Cell(x1=10, x2=20, y1=20, y2=30), Cell(x1=20, x2=30, y1=20, y2=30),
+                Cell(x1=0, x2=10, y1=30, y2=40), Cell(x1=10, x2=20, y1=30, y2=40), Cell(x1=20, x2=30, y1=30, y2=40),
+                Cell(x1=0, x2=10, y1=40, y2=100), Cell(x1=20, x2=30, y1=40, y2=100)]
+
+    lines = [Line(x1=0, x2=25, y1=12, y2=12),
+             Line(x1=0, x2=11, y1=31, y2=31),
+             Line(x1=13, x2=26, y1=29, y2=29),
+             Line(x1=13, x2=26, y1=38, y2=38),
+             Line(x1=17, x2=17, y1=2, y2=23)]
+
+    result = identify_table(line_group=line_group,
+                            column_delimiters=column_delimiters,
+                            elements=elements,
+                            lines=lines)
+
+    expected = Table(rows=[Row(cells=[Cell(x1=0, x2=10, y1=10, y2=30),
+                                      Cell(x1=10, x2=20, y1=10, y2=30),
+                                      Cell(x1=20, x2=30, y1=10, y2=30)]),
+                           Row(cells=[Cell(x1=0, x2=10, y1=30, y2=40, content=True),
+                                      Cell(x1=10, x2=20, y1=30, y2=40, content=True),
+                                      Cell(x1=20, x2=30, y1=30, y2=40, content=True)]),
+                           Row(cells=[Cell(x1=0, x2=10, y1=40, y2=100, content=True),
+                                      Cell(x1=10, x2=20, y1=40, y2=100, content=False),
+                                      Cell(x1=20, x2=30, y1=40, y2=100, content=True)]),
+                           ])
+
+    assert result == expected
