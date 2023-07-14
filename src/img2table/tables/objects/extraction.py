21f7316e8a3c78bcb801803281b0aea9d4163dbe
xavctn <90408365+xavctn@users.noreply.github.com>
2023-07-14 15:54:27 +0200

    Fix error in xlsx merged cells (#85)

--- src/img2table/tables/objects/extraction.py
+++ src/img2table/tables/objects/extraction.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from dataclasses import dataclass
-from typing import Optional, List, OrderedDict, NamedTuple
+from typing import Optional, List, OrderedDict, NamedTuple, Tuple
 
 import pandas as pd
 from xlsxwriter.format import Format
@@ -29,6 +29,48 @@ class CellPosition(NamedTuple):
     cell: TableCell
     row: int
     col: int
+
+
+def create_all_rectangles(cell_positions: List[CellPosition]) -> List[Tuple]:
+    """
+    Create all possible rectangles from list of cell positions
+    :param cell_positions: list of cell positions
+    :return: list of tuples representing rectangle coordinates
+    """
+    # Get bounding coordinates
+    min_col = min(map(lambda x: x.col, cell_positions))
+    max_col = max(map(lambda x: x.col, cell_positions))
+    min_row = min(map(lambda x: x.row, cell_positions))
+    max_row = max(map(lambda x: x.row, cell_positions))
+
+    # Get largest rectangle fully covered by cell positions
+    largest_area, area_coords, area_cell_pos = 0, None, None
+    for col_left in range(min_col, max_col + 1):
+        for col_right in range(col_left, max_col + 1):
+            for top_row in range(min_row, max_row + 1):
+                for bottom_row in range(top_row, max_row + 1):
+                    # Get matching cell positions
+                    matching_cell_pos = [cp for cp in cell_positions if col_left <= cp.col <= col_right
+                                         and top_row <= cp.row <= bottom_row]
+
+                    # Check if the rectangle is fully covered
+                    fully_covered = len(matching_cell_pos) == (col_right - col_left + 1) * (bottom_row - top_row + 1)
+
+                    # If rectangle is the largest, update values
+                    if fully_covered and (len(matching_cell_pos) > largest_area):
+                        largest_area = len(matching_cell_pos)
+                        area_cell_pos = matching_cell_pos
+                        area_coords = (col_left, top_row, col_right, bottom_row)
+
+    # Get remaining cell positions
+    remaining_cell_positions = [cp for cp in cell_positions if cp not in area_cell_pos]
+
+    if remaining_cell_positions:
+        # Get remaining rectangles
+        return [area_coords] + create_all_rectangles(remaining_cell_positions)
+    else:
+        # Return coordinates
+        return [area_coords]
 
 
 @dataclass
@@ -65,13 +107,16 @@ class ExtractedTable:
                 cell_pos = c.pop()
                 sheet.write(cell_pos.row, cell_pos.col, cell_pos.cell.value, cell_fmt)
             else:
-                # Case of merged cells
-                sheet.merge_range(first_row=min(map(lambda x: x.row, c)),
-                                  first_col=min(map(lambda x: x.col, c)),
-                                  last_row=max(map(lambda x: x.row, c)),
-                                  last_col=max(map(lambda x: x.col, c)),
-                                  data=c[0].cell.value,
-                                  cell_format=cell_fmt)
+                # Get all rectangles
+                for rect in create_all_rectangles(cell_positions=c):
+                    col_left, top_row, col_right, bottom_row = rect
+                    # Case of merged cells
+                    sheet.merge_range(first_row=top_row,
+                                      first_col=col_left,
+                                      last_row=bottom_row,
+                                      last_col=col_right,
+                                      data=c[0].cell.value,
+                                      cell_format=cell_fmt)
 
         # Autofit worksheet
         sheet.autofit()
--- tests/tables/objects/test_extraction.py
+++ tests/tables/objects/test_extraction.py
@@ -5,8 +5,24 @@ from io import BytesIO
 from xlsxwriter import Workbook
 
 from img2table.tables.objects.cell import Cell
+from img2table.tables.objects.extraction import create_all_rectangles, CellPosition, TableCell, BBox
 from img2table.tables.objects.row import Row
 from img2table.tables.objects.table import Table
+
+
+def test_create_all_rectangles():
+    c = TableCell(bbox=BBox(x1=0, y1=0, x2=0, y2=0), value="Test")
+    cell_positions = [CellPosition(cell=c, row=0, col=0), CellPosition(cell=c, row=1, col=0),
+                      CellPosition(cell=c, row=2, col=0), CellPosition(cell=c, row=3, col=0),
+                      CellPosition(cell=c, row=0, col=1), CellPosition(cell=c, row=1, col=1),
+                      CellPosition(cell=c, row=2, col=1), CellPosition(cell=c, row=3, col=1),
+                      CellPosition(cell=c, row=2, col=2), CellPosition(cell=c, row=3, col=2),
+                      CellPosition(cell=c, row=2, col=3), CellPosition(cell=c, row=3, col=3),
+                      ]
+
+    result = create_all_rectangles(cell_positions=cell_positions)
+
+    assert result == [(0, 0, 1, 3), (2, 2, 3, 3)]
 
 
 def test_extracted_table_worksheet():
