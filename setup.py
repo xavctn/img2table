04074286d33257167914c41536893e685a5dde55
Unay Shah <unayshah@gmail.com>
2024-12-08 17:49:37 -0800

    Added find_packages in setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     setup_requires=['pbr'],
+    packages=find_packages(where="src"),
     package_dir={'': 'src'},
     pbr=True,
 )
